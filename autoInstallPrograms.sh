48c2acf78dcfd9bf2444b773624a95ca7bb929e4
lucasmori <lucastecnew@gmail.com>
2019-06-01 03:38:55 -0300

    Resolved Issue

--- autoInstallPrograms.sh
+++ autoInstallPrograms.sh
@@ -25,17 +25,17 @@ echo Installing Programs...
 sleep 5
 
 snap install code --classic
-snap install postman -y
-snap install notepad-plus-plus -y
-snap install discord -y
+snap install postman
+snap install notepad-plus-plus
+snap install discord
 snap install slack --classic
-snap install telegram-desktop -y
+snap install telegram-desktop
 snap install code --classic
-snap install postman -y
-snap install notepad-plus-plus -y
-snap install discord -y
+snap install postman
+snap install notepad-plus-plus
+snap install discord
 snap install slack --classic
-snap install telegram-desktop -y
+snap install telegram-desktop
 apt-get install nodejs -y
 apt-get install npm -y
 apt-get install nvm -y
